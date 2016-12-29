53207c2fdda64e341a8232dad39ad388c73d8d7d
vpj20142015 <a10658@alunos.ipca.pt>
2016-12-29 16:52:38 +0000

    update

---
+++ Candido/Assets/enemy.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour
+{
+    Animator anim;
+    GameObject player;
+    Transform ppp;
+    public float speed = 2.0f;
+    public float damage = 10.0f;
+    public float hp = 40.0f;
+
+    //atatck stuff
+    float timer;
+    public float timeBetweenAttacks;
+
+    //state machine
+    bool isAttacking;
+    bool isDead;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        ppp = player.transform;
+        isAttacking = false;
+        isDead = false;
+        timer = 0.0f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (!isDead)
+        {
+            //move zombu
+            Move();
+            if (isAttacking)
+            {
+                if(timeBetweenAttacks <= timer)
+                    Attack();
+            }
+        }
+    }
+
+    //move
+    //the zombu will follow the player around
+    void Move()
+    {
+        Vector3 move = ppp.position - transform.position;
+        move.Normalize();
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider pl)
+    {
+        if (pl.CompareTag("Player"))
+        {
+            isAttacking = true;
+            anim.SetBool("attack_zombu", isAttacking);
+            anim.SetBool("move_zombu", false);
+        }
+    }
+
+    void OnTriggerExit(Collider pl)
+    {
+        if (pl.CompareTag("Player"))
+        {
+            isAttacking = false;
+            anim.SetBool("attack_zombu", isAttacking);
+            anim.SetBool("move_zombu", true);
+        }
+    }
+
+    //cause damage to the player
+    void Attack()
+    {
+        timer = 0.0f;
+        CharacterController plc = player.GetComponent<CharacterController>();
+      //  plc.TakeDamage(damage);
+        Debug.Log("AhAH I attakked u prayer");
+    }
+
+    //die
+    void Die()
+    {
+        isDead = true;
+        isAttacking = false;
+        anim.SetBool("Die_Zombie_die", isDead);
+        Destroy(gameObject, 1f);
+    }
+
+    //take damage
+    public void TakeDamage(float d)
+    {
+        hp -= d;
+        if (hp <= 0.0f)
+        {
+            Die();
+        }
+    }
+	
+}
