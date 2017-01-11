112ee535dec7caded95d9432fcff22da257138b6
Sangue3 <pedro_gomes96@sapo.pt>
2017-01-11 18:12:41 +0000

    Pasta Feito
    o trabalho final é a pasta feito

---
+++ feito/Assets/flashlight1.js
@@ -0,0 +1,31 @@
+﻿//Script made by StrupsGames
+
+var flashlight : Light;
+var soundOn : GameObject;
+var soundOff : GameObject;
+
+
+function Start () {
+flashlight.enabled = false; 
+}
+
+
+function Update () 
+{
+	
+	if(Input.GetKeyDown(KeyCode.F))	// You can choose any button, im using the button F, rename it if you want a diffrent button to use.
+	{
+		if(flashlight.enabled == false)
+		{
+			flashlight.enabled = true;
+			soundOn.GetComponent.<AudioSource>().Play();
+		}
+		else
+		{
+			flashlight.enabled = false;
+			soundOff.GetComponent.<AudioSource>().Play();
+
+		}
+	}
+}
+		
---
+++ feito/Assets/medkitRotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class medkitRotate : MonoBehaviour {
+
+    GameObject player;
+    public GameObject medkitManager;
+    interaction a;
+    bool setBool = false;
+    // Use this for initialization
+    void Start () {
+        //medkitManager = GameObject.FindGameObjectWithTag("manager");
+        a = medkitManager.GetComponent<interaction>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController plc = player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        if ((other.CompareTag("Player"))&&(plc.FullHealth()!=100))
+        {
+            
+            plc.GainHealth(20);
+            Debug.Log("COME SOPA, COME!");
+            a.ChangeMK(false);
+            Debug.Log("falsinho");
+            Destroy(GameObject.FindGameObjectWithTag("packerino"));
+            
+        }
+    }
+}
